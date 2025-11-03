0be1b60fe335d299e516f1c0c01820e19a3a0595
github-ci <you@example.com>
2025-11-03 14:17:22 +0800

    update++++++++++++

--- src/AzureOAuth2ClientBundle.php
+++ src/AzureOAuth2ClientBundle.php
@@ -9,6 +9,7 @@ use HttpClientBundle\HttpClientBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class AzureOAuth2ClientBundle extends Bundle implements BundleDependencyInterface
 {
@@ -18,6 +19,7 @@ class AzureOAuth2ClientBundle extends Bundle implements BundleDependencyInterfac
             DoctrineBundle::class => ['all' => true],
             MonologBundle::class => ['all' => true],
             HttpClientBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
